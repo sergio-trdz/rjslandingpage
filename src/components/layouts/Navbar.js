5b8832d17350b9d8ae7186a00eaa790b65c401ae
Sergio Trejo <sergiotrejordz@gmail.com>
2020-05-30 10:33:55 -0500

    adding pagenotfound and updating navbar

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,24 +4,27 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
- 
-
-  <div className="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul className="navbar-nav m-auto">
-      <li className="nav-item active">
-        <Link class="nav-link text-white text-uppercase ml-5" to="/">Home <span className="sr-only">(current)</span></Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link text-white text-uppercase ml-5" to="/projects">Projects</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link text-white text-uppercase ml-5" to="/contactme">Contact Me</Link>
-      </li>
-    </ul>
- 
-  </div>
-</nav>
-    );
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+      
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto">
+            <li className="nav-item active">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/">Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/projects">Projects</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white text-uppercase ml-5" to="/contactme">Contact Me</Link>
+            </li>
+          </ul>
+         
+        </div>
+      </nav>
+    )
 }
+
 
 export default Navbar;
---
+++ src/components/pages/PageNotFound.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class PageNotFound extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Ups that didn't work...</h2>
+            </div>
+        )
+    }
+}
+
+
+export default PageNotFound;
